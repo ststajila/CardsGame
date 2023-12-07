68e018d2c3ec90c8312c6d8d64eeacbf5040a7f0
Stanislav <stazilastas@gmail.com>
2023-12-07 09:51:42 -0600

    Game, shuffle and distribute cards, Keep Score, Updated gameViewController Design

--- LuckyCardsGame/GameViewController.swift
+++ LuckyCardsGame/GameViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-public class Deck{
+public class Card{
     var value: Int
     var suit: String
     
@@ -19,38 +19,98 @@ public class Deck{
 
 
 class GameViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var countLabel: UILabel!
+    
+    var userPoints = 0
+    var computerPoints = 0
+    
     @IBOutlet weak var playerImageView: UIImageView!
     
     @IBOutlet weak var computerImageView: UIImageView!
     
-    var deck: [Deck] = []
+    var deck: [Card] = []
+    var userDeck: [Card] = []
+    var computerDeck: [Card] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        for i in 1..<14 {
-            deck.append(Deck(value: i+1, suit: "spades"))
-            deck.append(Deck(value: i+1, suit: "hearts"))
-            deck.append(Deck(value: i+1, suit: "diamonds"))
-            deck.append(Deck(value: i+1, suit: "clubs"))
-        }
+        countLabel.text = "\(userPoints) - \(computerPoints)"
         
-        for val in deck{
-            print("\(val.value) " + val.suit)
-        }
+        deck = populateTheDeck()
+        deck.shuffle()
         
+        for i in 0..<26{
+            userDeck.append(deck[i])
+        }
+        for i in 26..<52{
+            computerDeck.append(deck[i])
+        }
+
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func play(_ sender: Any) {
+        if userDeck.count != 0 && computerDeck.count != 0{
+            playerImageView.image = UIImage(named: convertNametoImageId(card: userDeck[0]))
+            computerImageView.image = UIImage(named: convertNametoImageId(card: computerDeck[0]))
+            
+            if userDeck[0].value > computerDeck[0].value{
+                userDeck.append(userDeck.remove(at: 0))
+                var temp = computerDeck.remove(at: 0)
+                userDeck.append(temp)
+                userPoints += 1
+                countLabel.text = "\(userPoints) - \(computerPoints)"
+            } else if computerDeck[0].value > userDeck[0].value{
+                computerDeck.append(computerDeck.remove(at: 0))
+                var temp = userDeck.remove(at: 0)
+                computerDeck.append(temp)
+                computerPoints += 1
+                countLabel.text = "\(userPoints) - \(computerPoints)"
+            } else{
+                computerDeck.remove(at: 0)
+                userDeck.remove(at: 0)
+                
+                userPoints += 1
+                computerPoints += 1
+                countLabel.text = "\(userPoints) - \(computerPoints)"
+            }
+            
+            
+            
+        } else{
+            print("Game Over")
+        }
     }
-    */
-
-}
+    
+        
+    func populateTheDeck() -> [Card]{
+            var deckOfCards: [Card] = []
+            for i in 1..<14 {
+                deckOfCards.append(Card(value: i+1, suit: "spades"))
+                deckOfCards.append(Card(value: i+1, suit: "hearts"))
+                deckOfCards.append(Card(value: i+1, suit: "diamonds"))
+                deckOfCards.append(Card(value: i+1, suit: "clubs"))
+            }
+            return deckOfCards
+        }
+        
+        
+    func convertNametoImageId(card: Card) -> String{
+            if card.value < 11{
+                return "\(card.value)_of_\(card.suit).png"
+            }else{
+                if card.value == 11{
+                    return "jack_of_\(card.suit)2.png"
+                } else if card.value == 12{
+                    return "queen_of_\(card.suit)2.png"
+                } else if card.value == 13{
+                    return "king_of_\(card.suit)2.png"
+                } else{
+                    return "ace_of_\(card.suit).png"
+                }
+            }
+        }
+        
+    }
