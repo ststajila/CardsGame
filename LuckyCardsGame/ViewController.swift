f433c10b9b3d1fd05d3104c365ca11b2934346bf
Stanislav <stazilastas@gmail.com>
2023-12-18 09:53:55 -0600

    Programming Concepts and UI Elements

--- LuckyCardsGame/ViewController.swift
+++ LuckyCardsGame/ViewController.swift
@@ -1,9 +1,28 @@
-//
-//  ViewController.swift
-//  LuckyCardsGame
-//
-//  Created by STANISLAV STAJILA on 12/5/23.
-//
+/*Programming Concepts:
+1. Operators
+2. if/else statements
+3. While loop
+4. Arrays
+5. Functions
+6. Classes
+7. Switch
+8. Persistance
+9. Protocols
+10. If let statements
+11. App Icon
+*/
+/*UI Elements:
+1. Button
+2. Label
+3. Image View
+4. View Controller
+5. Alert Controller
+6. Stack View
+7. Table View
+8. Navigation Bar
+9. Navigation Controller
+ */
+
 
 import UIKit
 
